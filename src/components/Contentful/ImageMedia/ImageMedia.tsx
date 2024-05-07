7aa02284209c222df05d60b771cd511eca0f181c
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-07 16:49:05 -0700

    Add ImageMedia component to handel images from Contentful

---
+++ src/components/Contentful/ImageMedia/ImageMedia.tsx
@@ -0,0 +1,31 @@
+
+import * as React from "react";
+import "./ImageMedia.css"
+import Image from "next/image";
+
+interface Props {
+  src: string;
+  description?: string;
+  alt?: string;
+}
+
+const ImageMedia: React.FC<Props> = (props) => {
+  return (
+    <div className="BlogPost__ImageMedia__Container">
+      <img
+        src={"https:" + props.src}
+        alt={props.alt ? props.alt : "thumbnail"}
+        title={props.alt}
+        className="BlogPost__ImageMedia m-auto flex"
+        loading="lazy"
+      />
+      {props.description !== undefined && (
+        <em className="BlogPost__ImageMedia__Description mx-8 my-2 italic flex text-center items-center justify-center text-base">
+          {props.description}
+        </em>
+      )}
+    </div>
+  );
+};
+
+export default ImageMedia;
