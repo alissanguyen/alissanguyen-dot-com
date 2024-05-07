3217fd366194220f5ea7c0b02b8239949f24afaf
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-07 16:47:18 -0700

    Add SearchBarSection, SubscriptionForm, and TagsSection components on /blog route/;

---
+++ src/components/Blog/SearchBarSection.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import * as React from "react";
+import { DocumentTextIcon } from "@heroicons/react/24/outline";
+import SubscriptionForm from "./SubscriptionForm";
+import { useTheme } from "@/providers/ThemeProvider";
+
+/*
+* TODO: Enable Subscribing to newsletter
+*/
+
+interface Props {
+  search: string;
+  setSearch: (input: string) => void;
+  email: string;
+  setEmail: (input: string) => void;
+  count: number;
+}
+
+const SearchBarSection: React.FC<Props> = (props) => {
+  const { theme } = useTheme();
+  return (
+    <div className="BlogPage__Header__Wrapper">
+      <div className="">
+        <p className="BlogPage__SubHeader Slogan mb-3 xs:mb-5 gradient-text">
+          Find resources for <br className="hidden xs:flex"></br>web development and more.
+        </p>
+        <p className="text-subText opacity-75 text-lg xs:text-xl md:text-2xl mb-8">Tag along with me as I explore new tech and share my learning along the way!</p>
+        <div className="SearchBar__InputWrapper">
+          <input
+            type="text"
+            name="Search blog posts"
+            id="blog-post-search-bar"
+            value={props.search}
+            onChange={(e) => {
+              props.setSearch(e.target.value);
+            }}
+            className="SearchBar__Input"
+            placeholder="Search posts"
+          />
+          <div className="SearchBar__Icon">
+            <span className="mr-2">
+              <DocumentTextIcon className="h-5 w-5" />
+            </span>
+            <p className="text-sm opacity-80">{props.count}</p>
+          </div>
+        </div>
+        {/* <SubscriptionForm subscriberEmail={props.email} setSubscriberEmail={props.setEmail} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default SearchBarSection;
---
+++ src/components/Blog/SubscriptionForm.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { useTheme } from '@/providers/ThemeProvider';
+import { SupportedTheme } from '@/types';
+import * as React from 'react';
+
+interface Props {
+    subscriberEmail: string
+    setSubscriberEmail: (email: string) => void;
+}
+// TODO: implement this
+const SubscriptionForm: React.FC<Props> = (props) => {
+    const { theme } = useTheme();
+    return (
+        <form
+            id={"email-subscription-form"}
+            method="post"
+            action="/blog"
+            className="contact-form flex flex-col text-contact-label w-full"
+        >
+            <p className="BlogPage__SubHeader mb-5 leading-normal text-post-bodyText text-2xl font-normal">
+                Stay up to date with new tech, articles, and more!
+            </p>
+            <div className="flex flex-row gap-2 items-center">
+                <div className="SubscribeEmail__InputWrapper flex-grow">
+                    <input
+                        type="email"
+                        name="Subscribe"
+                        id="subscribe-email"
+                        value={props.subscriberEmail}
+                        onChange={(e) => {
+                            props.setSubscriberEmail(e.target.value);
+                        }}
+                        className="SubscribeEmail__Input"
+                        placeholder="Enter your email" />
+                </div>
+                <button type="submit" className={`Subscribe__Button rounded-xl text-lg px-5 h-12 bg-blog-tagBg ${theme === SupportedTheme.LIGHT ? 'text-black' : 'text-white'}`}>Subscribe</button>
+            </div>
+            {/* <div className="error-message">
+                Not a valid email
+            </div> */}
+        </form>
+    )
+}
+
+export default SubscriptionForm
