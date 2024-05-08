6e725577ba8f10bac3e00bdd066e1f1d44d3fa6e
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-07 23:45:47 -0700

    Add ProgressBar component

---
+++ src/components/FloatingHeader/ProgressBar.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import * as React from "react";
+
+const ProgressBar: React.FC<{ progress: number }> = ({ progress }) => {
+  return (
+    <div
+      className={`scroll-line`}
+      style={{
+        width: `${progress}%`
+      }}
+      id="scroll-line"
+    ></div>
+  );
+};
+
+export default ProgressBar;
