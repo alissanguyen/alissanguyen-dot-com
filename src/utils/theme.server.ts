c9f3fba4d77ef399a475ec131b1c91c4dd84a5a8
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-07 23:45:26 -0700

    Update old logic to new logic for setting theme to local storage

--- src/utils/theme.server.ts
+++ src/utils/theme.server.ts
@@ -1,39 +1,30 @@
-import { createCookieSessionStorage } from "@remix-run/node";
-import { THEME_COOKIE_KEY } from "~/constants";
-import { SupportedTheme } from "~/types";
+import { SupportedTheme } from "@/types";
+
+const THEME_KEY = "alissa_nguyen_com_theme";
 
 const sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) {
   throw new Error("SESSION_SECRET must be set");
 }
 
-
-
-const themeStorage = createCookieSessionStorage({
-  cookie: {
-    name: "alissa_nguyen_dev_theme",
-    secure: true,
-    secrets: [sessionSecret],
-    sameSite: "lax",
-    path: "/",
-    httpOnly: true
+export function getTheme(): SupportedTheme {
+  if (typeof window !== "undefined") {
+    const storedTheme = localStorage.getItem(THEME_KEY);
+    if (isTheme(storedTheme)) {
+      return storedTheme;
+    }
   }
-});
-
-export async function getThemeSession(request: Request) {
-  const session = await themeStorage.getSession(request.headers.get("Cookie"));
-  return {
-    getTheme: () => {
-      const themeValue = session.get(THEME_COOKIE_KEY);
-      return isTheme(themeValue) ? themeValue : SupportedTheme.LIGHT;
-    },
-    setTheme: (theme: SupportedTheme) => session.set(THEME_COOKIE_KEY, theme),
-    commit: () => themeStorage.commitSession(session)
-  };
+  return SupportedTheme.LIGHT;
 }
 
-export const isTheme = (maybeTheme: unknown): maybeTheme is SupportedTheme => {
+export function setTheme(theme: SupportedTheme): void {
+  if (typeof window !== "undefined") {
+    localStorage.setItem(THEME_KEY, theme);
+  }
+}
+
+export function isTheme(maybeTheme: unknown): maybeTheme is SupportedTheme {
   return (
     maybeTheme === SupportedTheme.DARK || maybeTheme === SupportedTheme.LIGHT
   );
-};
+}
