70d8e8b5030deb5a15ab982a064ee3ad71593d94
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-08 13:44:05 -0700

    Move useEffect to MobileMenu component to fix React hook rule

--- src/components/NavBar/MobileMenu.tsx
+++ src/components/NavBar/MobileMenu.tsx
@@ -26,61 +26,57 @@ const getClassName = (
     return "text-gray-500 hover:text-white border-gray-500 hover:border-white focus:border-white";
   }
 };
+
+
 const MobileMenu: React.FC<NavbarProps> = (props) => {
   const { modalIsOpen, updateModalStatus } = useModalContext();
 
-  const className = getClassName(
-    props.theme,
-    modalIsOpen
-  );
+  const className = getClassName(props.theme, modalIsOpen);
+
+
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  React.useEffect(() => {
+    updateModalStatus(isExpanded);
+  }, [isExpanded, updateModalStatus]);
+  
   return (
     <Menu>
-      {({ isExpanded }) => {
-        // TODO: Fix this
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        React.useEffect(() => {
-          updateModalStatus(isExpanded);
-        }, [isExpanded]);
-
-        return (
-          <>
-            <MenuButton
-              id="menu--1"
-              aria-label={isExpanded ? "Close menu" : "Open menu"}
-              name={isExpanded ? "Close menu" : "Open menu"}
-              className={
-                "focus:outline-none inline-flex h-12 w-12 items-center justify-center rounded-full border-2 p-1 transition " +
-                className
-              }
+      {() => (
+        <>
+          <MenuButton
+            id="menu--1"
+            aria-label={isExpanded ? "Close menu" : "Open menu"}
+            name={isExpanded ? "Close menu" : "Open menu"}
+            className={
+              "focus:outline-none inline-flex h-12 w-12 items-center justify-center rounded-full border-2 p-1 transition " +
+              className
+            }
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            <svg
+              className={`ham hamRotate ham8 ${modalIsOpen ? "active" : ""}`}
+              viewBox="0 0 100 100"
+              width="80"
+              id="ham-button"
+              fill="currentColor"
             >
-              <svg
-                className={`ham hamRotate ham8 ${modalIsOpen ? "active" : ""}`}
-                viewBox="0 0 100 100"
-                width="80"
-                id="ham-button"
+              <path
+                className="line top"
+                d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
                 fill="currentColor"
-              >
-                <path
-                  className="line top"
-                  d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
-                  fill="currentColor"
-                />
-                <path
-                  className="line middle"
-                  d="m 30,50 h 40"
-                  fill="currentColor"
-                />
-                <path
-                  className="line bottom"
-                  d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
-                  fill="currentColor"
-                />
-              </svg>
-            </MenuButton>
-            <MobileMenuList {...props} isExpanded={isExpanded} />
-          </>
-        );
-      }}
+              />
+              <path className="line middle" d="m 30,50 h 40" fill="currentColor" />
+              <path
+                className="line bottom"
+                d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+                fill="currentColor"
+              />
+            </svg>
+          </MenuButton>
+          <MobileMenuList {...props} isExpanded={isExpanded} />
+        </>
+      )}
     </Menu>
   );
 };
