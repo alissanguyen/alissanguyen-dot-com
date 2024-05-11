820a5a2927dda1fbcd3042b849ad7d9d9f287964
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-11 12:40:46 -0700

    Add Metadata to index route (home page)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,114 +1,87 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import NavBar from '@/components/NavBar/NavBar';
 import Footer from '@/components/Footer/Footer';
-import { usePathname } from 'next/navigation';
-import { SupportedTheme } from '@/types';
-import { ThemeContextProvider, useTheme } from '@/providers/ThemeProvider';
-import "../styles/globals.css"
+import { HtmlElement } from '@/components/HtmlElement';
+import NavBar from '@/components/NavBar/NavBar';
 import { ModalContextProvider } from '@/providers/ModalProvider';
-import { getTheme } from '@/utils/theme.server';
-import { AUTHOR, BLOG_DESCRIPTION, BLOG_IMAGE_URL, BLOG_KEYWORDS, BLOG_URL, BLOG_WEBSITE_NAME, fixedWidthLayoutClasses, IMAGE_HEIGHT, IMAGE_WIDTH, NAVBAR_ID, TWITTER_ACC, TWITTER_CARD_TYPE } from '@/constants';
+import { Metadata } from 'next';
+import React from 'react';
+import "../styles/globals.css";
+import { cookies } from 'next/headers';
+import { THEME_KEY, ThemeSchema } from '@/utils/theming';
+import { SupportedTheme } from '@/types';
+import { ThemeContextProvider } from '@/providers/ThemeProvider';
 
-const convertSupportedThemeToClassName = (
-  theme: SupportedTheme,
-  onBlogRoute: boolean
-): string => {
-  if (theme === SupportedTheme.LIGHT) {
-    if (onBlogRoute) {
-      return 'light-theme light-theme-blog';
-    }
-    return 'light-theme';
-  } else {
-    if (onBlogRoute) {
-      return 'dark-theme dark-theme-blog';
-    }
-    return 'dark-theme';
-  }
+
+export const WEBSITE_KEYWORDS =
+  "Learn Remix, React, JavaScript, Typescript, Alissa Nguyen Blog, Alissa Nguyen, Software Development, Software Engineer, Modern Programing, Frontend Engineer, Web Developer, AlissaNguyen, Seattle, Full-stack developer, Web development, Software engineering, Programming languages, Responsive web design, User experience (UX), User interface (UI), Front-end development, Back-end development, Web applications, Mobile-friendly websites.";
+
+const WEBSITE_DESCRIPTION =
+  "Hi, I'm Alissa. I'm a software engineer in Seattle with expertise in creating innovative and user-friendly web applications. I deliver high-quality, responsive websites that prioritize user experience. I enjoy building software with elegant, performant, and maintainable frontend code.";
+
+const WEBSITE_URL = "https://www.alissanguyen.com/";
+const PORTFOLIO_WEBSITE_NAME = "Alissa Nguyen";
+
+const IMAGE_WIDTH = "1200";
+const IMAGE_HEIGHT = "630";
+const TWITTER_ACC = "@ai_alissa";
+const TWITTER_CARD_TYPE = "summary_large_image";
+const PORTFOLIO_IMAGE_URL =
+  "http://www.alissanguyen.com/assets/images/preview.png";
+export const metadata: Metadata = {
+  title: PORTFOLIO_WEBSITE_NAME,
+  description: WEBSITE_DESCRIPTION,
+  keywords: WEBSITE_KEYWORDS,
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+  },
+  openGraph: {
+    title: PORTFOLIO_WEBSITE_NAME,
+    description: WEBSITE_DESCRIPTION,
+    url: WEBSITE_URL,
+    images: [
+      {
+        url: PORTFOLIO_IMAGE_URL,
+        width: IMAGE_WIDTH,
+        height: IMAGE_HEIGHT,
+        alt: PORTFOLIO_WEBSITE_NAME,
+      },
+    ],
+    locale: "en_US",
+    type: "website"
+  },
+
+  twitter: {
+    card: TWITTER_CARD_TYPE,
+    site: TWITTER_ACC,
+    creator: TWITTER_ACC,
+    title: PORTFOLIO_WEBSITE_NAME,
+    description: WEBSITE_DESCRIPTION,
+    images: [
+      {
+        url: PORTFOLIO_IMAGE_URL,
+        alt: PORTFOLIO_WEBSITE_NAME,
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { get } = cookies()
+  const maybeThemeCookie = get(THEME_KEY)?.value
 
-  const pathname = usePathname();
-  const isOnBlogRoute = pathname.startsWith('/blog');
-  const isOnSlugRoute = pathname.startsWith('/blog/');
+  const validatedTheme = ThemeSchema.safeParse(maybeThemeCookie).data
 
-  const [theme, setTheme] = useState<SupportedTheme>(SupportedTheme.DARK);
+  const resolvedTheme = validatedTheme ? validatedTheme : SupportedTheme.DARK
 
-  const [navbarOpacity, setNavbarOpacity] = useState(0);
 
-  useEffect(() => {
-    const storedTheme = getTheme();
-    setTheme(storedTheme);
-  }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-
-      const newOpacity = Math.min(scrollPosition / 200, 0.2);
-
-      setNavbarOpacity(newOpacity);
-
-      // Save the scroll position to localStorage
-      localStorage.setItem('scrollPosition', scrollPosition.toString());
-    };
-
-    // Check if there is a stored scroll position in localStorage
-    const savedScrollPosition = localStorage.getItem('scrollPosition');
-    if (savedScrollPosition) {
-      const scrollPosition = parseInt(savedScrollPosition);
-      window.scrollTo(0, scrollPosition);
-
-      // Calculate the initial opacity based on the stored scroll position
-      const initialOpacity = Math.min(scrollPosition / 200, 0.2);
-      setNavbarOpacity(initialOpacity);
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  // TODO: Fix metadata not rendering
   return (
-    <ThemeContextProvider initialTheme={theme}>
-      <ModalContextProvider>
-        <ThemeSetter>
-          {!isOnBlogRoute ? (<Head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Alissa Nguyen</title>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="manifest" href="/site.webmanifest" />
-          </Head>) : (
-            <Head>
-              <title>{BLOG_WEBSITE_NAME}</title>
-              <meta name="description" content={BLOG_DESCRIPTION} />
-              <meta name="keywords" content={BLOG_KEYWORDS} />
-              <meta property="og:title" content={BLOG_WEBSITE_NAME} />
-              <meta property="og:description" content={BLOG_DESCRIPTION} />
-              <meta property="og:url" content={BLOG_URL} />
-              <meta property="og:image" content={BLOG_IMAGE_URL} />
-              <meta property="og:image:width" content={IMAGE_WIDTH} />
-              <meta property="og:image:height" content={IMAGE_HEIGHT} />
-              <meta property="og:image:alt" content={BLOG_WEBSITE_NAME} />
-              <meta name="twitter:title" content={BLOG_WEBSITE_NAME} />
-              <meta name="twitter:description" content={BLOG_DESCRIPTION} />
-              <meta name="twitter:card" content={TWITTER_CARD_TYPE} />
-              <meta name="twitter:creator" content={TWITTER_ACC} />
-              <meta name="twitter:site" content={TWITTER_ACC} />
-              <meta name="twitter:image" content={BLOG_IMAGE_URL} />
-              <meta name="twitter:image:alt" content={BLOG_WEBSITE_NAME} />
-              <meta name="theme-color" content="#212529" />
-              <link rel="author" href={AUTHOR} />
-            </Head>
-          )}
+    <ModalContextProvider>
+      <ThemeContextProvider initialTheme={resolvedTheme}>
+        <HtmlElement initialTheme={resolvedTheme}>
           <body id="root">
             <script
               async
@@ -127,31 +100,18 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
                 </p>
               </div>
             </noscript>
-            {isOnSlugRoute ? (<div className={fixedWidthLayoutClasses + " mb-10 md:mb-16 lg:mb-20"} id={NAVBAR_ID}></div>) : <NavBar navbarOpacity={navbarOpacity} />}
+            <NavBar />
             <div className="Document__Content screen-body">{children}</div>
             <Footer />
           </body>
-        </ThemeSetter>
-      </ModalContextProvider>
-    </ThemeContextProvider >
+        </HtmlElement>
+      </ThemeContextProvider>
+    </ModalContextProvider>
+
   );
 };
 
-const ThemeSetter = (props: React.PropsWithChildren) => {
-  const { theme } = useTheme();
 
-  const pathname = usePathname();
-  const isOnBlogRoute = pathname.startsWith('/blog');
-
-  return (
-    <html
-      lang="en"
-      className={convertSupportedThemeToClassName(theme, isOnBlogRoute)}
-    >
-      {props.children}
-    </html>
-  )
-}
 
 export default Layout;
 
