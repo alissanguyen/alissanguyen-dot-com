8b5faa2506ba60d74154a765a0b3b568a5f7c162
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-11 14:09:46 -0700

    Update NavBar style

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -18,13 +18,26 @@ const Navbar: React.FC = (props) => {
   const currentTopLevelRoute = pathname;
   const isOnSlugRoute = pathname.startsWith('/blog/');
 
-  const [navbarOpacity, setNavbarOpacity] = React.useState(0);
+  const [navbarStyle, setNavbarStyle] = React.useState({
+    borderBottom: 'solid 2px var(--base01)',
+    backgroundColor: 'var(--background-nav)',
+  });
 
   React.useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
-      const newOpacity = Math.min(scrollPosition / 100, 0.3);
-      setNavbarOpacity(newOpacity);
+      if (scrollPosition > 50) {
+        setNavbarStyle({
+          borderBottom: 'solid 2px var(--base01)',
+          backgroundColor: 'var(--background-nav)',
+        });
+      } else {
+        setNavbarStyle({
+          borderBottom: 'none',
+          backgroundColor: 'transparent',
+        });
+      }
+
       // Save the scroll position to localStorage
       localStorage.setItem('scrollPosition', scrollPosition.toString());
     };
@@ -34,11 +47,14 @@ const Navbar: React.FC = (props) => {
     if (savedScrollPosition) {
       const scrollPosition = parseInt(savedScrollPosition);
       window.scrollTo(0, scrollPosition);
-      // Calculate the initial opacity based on the stored scroll position
-      const initialOpacity = Math.min(scrollPosition / 100, 0.8);
-      setNavbarOpacity(initialOpacity);
+      // Set the initial navbar style based on the stored scroll position
+      if (scrollPosition > 50) {
+        setNavbarStyle({
+          borderBottom: '1px solid gray',
+          backgroundColor: 'white',
+        });
+      }
     }
-
     window.addEventListener('scroll', handleScroll);
 
     return () => {
@@ -46,60 +62,63 @@ const Navbar: React.FC = (props) => {
     };
   }, []);
 
+  // max-w-screen-lg
+  // max-w-screen-xl pr-5 
   return (
-    isOnSlugRoute ? (<div className={fixedWidthLayoutClasses + " mb-10 md:mb-16 lg:mb-20"} id={NAVBAR_ID} ></div >) : (<>
+    isOnSlugRoute ? (<div className={" mb-10 md:mb-16 lg:mb-20"} id={NAVBAR_ID} ></div >) : (<>
       <div
-        className={fixedWidthLayoutClasses + " mb-10 md:mb-16 lg:mb-20"}
+        className={" mb-10 md:mb-16 lg:mb-20"}
         id={NAVBAR_ID}
 
       >
-        <nav className="NavBar__Wrapper max-w-screen-lg flex flex-row relative" >
-          <div className="NavBar__InnerWrapper fixed top-[2rem] bg-blue-10 z-[100] flex flex-row gap-40 items-center justify-between w-full max-w-screen-xl rounded-lg left-[0] xl:left-auto pl-8 sm:pl-12 pb-15 pr-5 xl:px-0 p-8 lg:p-0 lg:pl-5" style={{
-            backgroundColor: `rgba(107, 107, 107, ${navbarOpacity})`,
-            backdropFilter: `blur(${navbarOpacity * 50}px)`,
-          }}>
-            <div>
-              <NavLogo
-                isCurrentRoute={getIsActiveRoute(
-                  "/",
-                  currentTopLevelRoute === undefined ? "" : currentTopLevelRoute
-                )}
-              />
-            </div>
+        <nav className="NavBar__Wrapper flex flex-row relative"  >
+          <div className="NavBar__InnerWrapper fixed z-[100] flex w-full left-[0] xl:left-auto mb-10" style={navbarStyle}>
+            <div className="flex flex-row items-center justify-between w-full gap-40 max-w-screen-xl mx-auto p-8 sm:p-12 pb-10 lg:px-0 lg:pb-5 lg:pt-14">
+              <div>
+                <NavLogo
+                  isCurrentRoute={getIsActiveRoute(
+                    "/",
+                    currentTopLevelRoute === undefined ? "" : currentTopLevelRoute
+                  )}
+                />
+              </div>
+              <nav className="Dekstop__NavBar hidden lg:flex list-none w-full justify-right">
+                <div className="flex flex-row items-center justify-between w-full">
+                  {topLevelLinksOnDesktop.map((link) => {
+                    return (
 
-            <nav className="Dekstop__NavBar hidden lg:flex list-none w-full justify-right">
-              <div className="flex flex-row items-center justify-between w-full">
-                {topLevelLinksOnDesktop.map((link) => {
-                  return (
-
-                    <NavLink
-                      to={link.href}
-                      key={link.href}
-                      href={link.href}
-                      isCurrentRoute={getIsActiveRoute(
-                        link.href,
-                        currentTopLevelRoute === undefined
-                          ? ""
-                          : currentTopLevelRoute
-                      )}
-                    >
-                      {link.displayName}
-                    </NavLink>
+                      <NavLink
+                        to={link.href}
+                        key={link.href}
+                        href={link.href}
+                        isCurrentRoute={getIsActiveRoute(
+                          link.href,
+                          currentTopLevelRoute === undefined
+                            ? ""
+                            : currentTopLevelRoute
+                        )}
+                      >
+                        {link.displayName}
+                      </NavLink>
 
 
-                  );
-                })}
-                <div className="noscript-hidden hidden lg:block">
-                  <ThemeButton />
+                    );
+                  })}
+                  <div className="noscript-hidden hidden lg:block">
+                    <ThemeButton />
+                  </div>
+                </div>
+              </nav>
+
+              <div className="Mobile__NavBar fixed right-[2rem] sm:right-[3rem] flex items-center justify-center z-[200]">
+                <div className="block lg:hidden">
+                  <MobileMenu theme={theme} />
                 </div>
               </div>
-            </nav>
-
-            <div className="Mobile__NavBar fixed right-[2rem] sm:right-[3rem] flex items-center justify-center z-[200]">
-              <div className="block lg:hidden">
-                <MobileMenu theme={theme} />
-              </div>
             </div>
+
+
+
           </div>
         </nav>
       </div>
