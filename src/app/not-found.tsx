29f989bb473e0f203b1dbad060df292819880ed1
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-13 14:55:07 -0700

    Add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client'
 import * as React from 'react';
+import "../styles/notfound.css"
 
 interface Props {
 
@@ -7,8 +9,20 @@ interface Props {
 
 const NotFoundPage: React.FC<Props> = ({ }) => {
     return (
-        <div>
-            <p>The page you are trying to go to does not exist. Please try again.</p>
+        <div className='NotFound__Page'>
+            <h1>404</h1>
+            <div className="cloak__wrapper">
+                <div className="cloak__container">
+                    <div className="cloak"></div>
+                </div>
+            </div>
+            <div className="info">
+                <h2 className='mb-2 text-lg text-cyan-400'>We can't find that page :(</h2>
+                <p className='text-base'>
+                    We're fairly sure that page used to be here, but seems to have gone missing. We do apologise on it's behalf.
+                </p>
+                <a href="/" className='ease-in-out duration-300'>Home</a>
+            </div>
         </div>
     )
 }
